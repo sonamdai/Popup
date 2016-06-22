291c94cfb58e93725770ff411150bf9bedf9a5cd
Sonam <Sonam>
2016-06-22 10:29:24 +0545

    hello guys this is my first commit

---
+++ app/src/main/java/com/example/nirmal/pop/Second.java
@@ -0,0 +1,27 @@
+package com.example.nirmal.pop;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.view.View;
+import android.widget.Button;
+
+/**
+ * Created by nirmal on 6/16/2016.
+ */
+public class Second extends MainActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.second);
+
+        DisplayMetrics dm = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(dm);
+        int width = dm.widthPixels;
+        int height = dm.heightPixels;
+        getWindow().setLayout((int)(width*.8),(int)(height*.6));
+
+
+
+        }
+}
